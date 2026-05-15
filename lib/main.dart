3f3a9876d7a8ae64b84cbfdbb4e1cf74e61bcd9f
shaaf6 <109850071+shaaf6@users.noreply.github.com>
2026-05-15 23:43:26 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,11 @@ import 'package:flutter/material.dart';
 import 'app.dart';
 import 'core/di/injection.dart';
 
+//group 5 
+//muhammad shahaf k22-4230
+//muhammad taha wala k22-4312
+//muhammad ai zuberi k22-4177
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   // google-services.json handles native init; this just bridges the Dart layer.
